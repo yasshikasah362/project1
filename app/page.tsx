d2d2fbdc20b1a7f8926c56f5f3f7bc07061f01fb
yashika <yashikasah362@gmail.com>
2025-04-10 12:51:29 +0530

    in hero section changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 'use client'
 import React, { useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 import Nav from './components/Navbar/Nav';
 import Home from './components/Home';
 
 
 
 const Page = () => {
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap.bundle.min.js')
-      .then((bootstrap) => {
-        // Bootstrap loaded successfully
-      })
-      .catch((err) => console.error("Bootstrap JS loading failed", err));
-  }, []);
+  
   
 
   return (
--- declarations.d.ts
+++ declarations.d.ts
@@ -1,3 +1,3 @@
 // declarations.d.ts
-declare module 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// declare module 'bootstrap/dist/js/bootstrap.bundle.min.js';
 declare module 'react-slick';
